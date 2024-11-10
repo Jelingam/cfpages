d5e07dec03d0968ff833c1af2bd4f4dd9307d595
Bude Jellingham <89634871+Jelingam@users.noreply.github.com>
2024-11-10 14:14:19 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'www.naser.roozegaregharibist.sbs'
+      url.hostname = '193.187.172.112'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
